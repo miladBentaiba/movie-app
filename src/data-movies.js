e1038f46eceb5afca9f0fe801f09c78678cc4f98
miladBentaiba <bm_bentaiba@esi.dz>
2019-05-20 23:46:12 +0100

    add movies data

---
+++ src/data-movies.js
@@ -0,0 +1,51 @@
+export default movies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    image: 'https://m.media-amazon.com/images/M/MV5BMTA1MjE0Nzk4MDleQTJeQWpwZ15BbWU4MDA0NjIxMjAx._V1_.jpg',
+    ranking: 9.3
+  },{
+    title: 'The Godfather',
+    year: 1972,
+    description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+    image: 'https://m.media-amazon.com/images/M/MV5BMTgyODUzMjM2MV5BMl5BanBnXkFtZTYwOTA3MDU2._V1_.jpg',
+    ranking: 9.2
+  },{
+    title: 'The Lord of the Rings: The Return of the King',
+    year: 2003,
+    description: 'Gandalf and Aragorn lead the World of Men against Sauron\'s army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.',
+    image: 'https://m.media-amazon.com/images/M/MV5BMTI5MDg3MzU1MV5BMl5BanBnXkFtZTcwODk2MTk2Mw@@._V1_SY1000_CR0,0,1751,1000_AL_.jpg',
+    ranking: 8.9
+  },{
+    title: 'Terminator 2: Judgment Day',
+    year: 1991,
+    description: 'A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her teenage son, John Connor, from a more advanced and powerful cyborg.',
+    image: 'https://m.media-amazon.com/images/M/MV5BMTU2NTc2NDAwNF5BMl5BanBnXkFtZTgwNjk4MzgwMzI@._V1_SX1324_CR0,0,1324,999_AL_.jpg',
+    ranking: 8.5
+  },{
+    title: 'Taxi Driver',
+    year: 1976,
+    description: 'A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action by attempting to liberate a presidential campaign worker and an underage prostitute.',
+    image: 'https://m.media-amazon.com/images/M/MV5BMTU1NDQyNDk3Ml5BMl5BanBnXkFtZTgwNjUwNzg1MDI@._V1_SX1252_CR0,0,1252,999_AL_.jpg',
+    ranking: 8.3
+  },{
+    title: 'Le crime était presque parfait',
+    year: 1954,
+    description: 'A tennis player frames his unfaithful wife for first-degree murder after she inadvertently hinders his plan to kill her.',
+    image: 'https://m.media-amazon.com/images/M/MV5BNDYxNjA4MDg2M15BMl5BanBnXkFtZTcwNjQyODMwOQ@@._V1_SX1777_CR0,0,1777,999_AL_.jpg',
+    ranking: 8.2
+  },{
+    title: 'Gran Torino',
+    year: 2008,
+    description: 'Disgruntled Korean War veteran Walt Kowalski sets out to reform his neighbor, a Hmong teenager who tried to steal Kowalski\'s prized possession: a 1972 Gran Torino.',
+    image: 'https://m.media-amazon.com/images/M/MV5BMjA4NTc4ODA3MF5BMl5BanBnXkFtZTcwNzUwMzUxMg@@._V1_.jpg',
+    ranking: 8.1
+  },{
+    title: 'Tu ne tueras point',
+    year: 2016,
+    description: 'World War II American Army Medic Desmond T. Doss, who served during the Battle of Okinawa, refuses to kill people, and becomes the first man in American history to receive the Medal of Honor without firing a shot.',
+    image: 'https://m.media-amazon.com/images/M/MV5BNWVhYjBmZjUtNzY5Ny00NzZiLTg2YzEtNDVlMGY5ZGIxMWQxXkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_SY1000_CR0,0,666,1000_AL_.jpg',
+    ranking: 8.1
+  }
+]
