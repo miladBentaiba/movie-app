dfb09387dd0d979d730b9ac8254e348498c39195
miladBentaiba <bm_bentaiba@esi.dz>
2019-05-20 23:11:17 +0100

    filter form finished

--- src/Inputs.js
+++ src/Inputs.js
@@ -1,6 +1,6 @@
 import React  from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
-import { Collapse, Input, } from 'reactstrap';
+import { Collapse, FormGroup, Input, Label, Form} from 'reactstrap';
 
 export default class Inputs extends React.Component {
   constructor(props) {
@@ -15,13 +15,26 @@ export default class Inputs extends React.Component {
 
   render() { 
     return (  
-      <div className="container">
-        <i onClick={this.toggle} className={this.state.up?'fas fa-sort-down':'fas fa-sort-up'}></i>
+      <div className="container all-form">
+        <div onClick={this.toggle} className='toggler-parent'>
+          <i  className={'float-right ' +(this.state.up?'fas fa-sort-down':'fas fa-sort-up')}></i>
+        </div>
         <Collapse isOpen={this.state.collapse}>
-          <Input type="email" name="title" id="title" placeholder="Search by title" />
-          <Input type="number" name="year" id="year" placeholder="Search by year" />
-          <Input type="number" name="ratingHigh" id="exampleDatetime" placeholder="Search by rating higher than" />
-          <Input type="number" name="ratingLow" id="exampleDatetime" placeholder="Search by rating lower than" />
+          <Form>
+            <FormGroup>
+              <Label for="exampleCheckbox float-left">Title</Label>
+              <Input type="text" name="title" id="title" placeholder="Search by title" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleCheckbox float-left">Year</Label>
+              <Input type="number" name="year" id="year" placeholder="Search by year" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleCheckbox float-left">Ranking</Label>
+              <Input type="number" name="ratingHigh" id="exampleDatetime" placeholder="Search by rating higher than" />
+              <Input type="number" name="ratingLow" id="exampleDatetime" placeholder="Search by rating lower than" />
+            </FormGroup>
+          </Form>
         </Collapse>
       </div>
     );
